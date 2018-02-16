46cd051d353b37021be89412708b8acf80173fcd
Chris Swanner <cmanswanner@gmail.com>
2018-02-16 12:36:10 -0800

    add views and azor code to grab the input from the user and display it

--- WordCounter/Controllers/HomeController.cs
+++ WordCounter/Controllers/HomeController.cs
@@ -5,6 +5,25 @@ using WordCounter.Models;
 
 namespace WordCounter.Controllers
 {
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+    [HttpPost("/checkword")]
+    public ActionResult CheckWord()
+    {
+      string searchWord = Request.Form["word"];
+      string phrase = Request.Form["phrase"];
+      int count = WordModel.CountWords(searchWord, phrase);
+
+      WordModel Model = new WordModel(searchWord, phrase, count);
+
+      return View(Model);
+    }
+  }
 
 
 }
